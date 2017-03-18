369d2b5701cefb6afaf9194a0fd7f903b3357543
Retidurc SilverNight <retidurc@gmail.com>
2017-03-18 17:49:13 +0100

    Lol

---
+++ insultotron.php
@@ -0,0 +1,74 @@
+﻿<?php
+function animal()
+{
+ $array = array(
+	"e Végéta",
+	"UNDEFINED",
+	"e renard",
+	"e poulpe",
+	"e caribou",
+	"e chouette",
+	"'ornythorinque",
+	"e papier toilette",
+	"e zombie nécrophage",
+	"e vélocipède",
+	"e castor",
+	"e mamouth a poil ras",
+	"e mamouth",
+	"'huitre",
+	"e joueur de LoL"
+	);
+echo $array[mt_rand(0,count($array)-1)];
+
+}
+
+function lulz()
+{
+ $array = array(
+	"moustachu",
+	"barbu",
+	"roux",
+	"psychopathe",
+	"reconditioné",
+	"chauve",
+	"shootéa l'ibuprofen",
+	"nucléairement con",
+    "mangeur de mamouths",
+	"manchot",
+	"pyroclastique",
+	"associal"
+	);
+	echo$array[mt_rand(0,count($array)-1)];
+}
+
+function lieu()
+{
+ $array = array(
+	"des iles",
+	"venant de mars",
+	"des galapagos",
+	"australien",
+	"de ta soeur",
+	"sumérien",
+	"grec",
+	"de l'espace",
+	"du Zimbabwe",
+	"italien",
+	"des antilles",
+	"numériquement à la ramasse"
+//	"OMG c'est quoi ce bPAaaar !  ͫ͌ṵ̮̲̣͊͊̐̽̊̅̚͠t̹͈̩ͧ̆̐a̡ͪ͌ͬ̀̂̾̚i̳͙̒͊̎͋̉̐̚n̠̞̂̄̇ ͓͚̩̜ͅċ͔̜̳̗̭͖͑ͫ'̪͔̗͙̩͈̎̏̚ͅề̟̅̅ͧs̛͉͉̆t̫̥̆̇ͥ͐̊͞ ̒͐̑̈́̈ͣ̚͏̮͚̱̬̺q̸͇̮͔ȕ̮̦͇̜̝̌ͯ͗͡o̜ͮ̈̏̔̈́ỉ̤̟ͧͯ́͠ ̄c̪̞͛̄ͬ̿́e̼̦̥̗̒ ̼̯̬͈͚͍̼ͮ̒b̵͎͉͍͚ͪ̒ͩͭo͖̺ͨͫ̊ͮ̿ȑ̩̲͑̈́͂d̸̖̬̞̦̘̯̱͑e̞̖̍ͧ̏ͨ̒̀l̪̬͋̊͛͌ͧ̽ ̖̤̘̮͕͆̀͗ͩͯ̑ͅͅd̼̝̋̃ͮ̏ͫͭ̈e͉̮͚̠̫̯ͮ͂̈́̒̈ ̤̯̖̝̜̉̌A̧ͩ́ͧ̈́̂̚A͎̳̰̪͎͉A͚̪ͬͩ̈ͮ̓R͙̰͂̓̌ͮ̌ͯ͠G̝̰̺̺̪͆̉ͦ̈́͢H͕̋̔́̑͟ ̰̬͚̃͆̅̉͑̃̐ il me  ͢ ̶͓̜͗ ̳̹͇̱̼͈͕͆̃̿ ̴̀ͧͯ ̾ͧͪ͑҉̪͙͓̜̟̖ ̙̤̼̋͗ ̲̯͈̰̫͍ ̖̲͍ͬ̿̂̒̇̈ ̤̻̍̃͊ͨͫ̚ ͓͇̩̝̝̃ͅ ̧͉̥͔̞͓̥̝̊̇ ̳̫̊̽̑ ̎ ̜̯̻̗̖͎̘͂̂̃͛ ̺̟͍̙̔ͫͪͫ͛ͯ͝ ͬ̔ ̵̑̑ͯ́̀ͤ̓MANGȩ̸̴̼̟̘̪̖͎̹͚̬̹͙̹̤̹͍̤̫ͪͦ͆ͩ͊̎͊̕͘",
+	
+	);
+	echo $array[mt_rand(0,count($array)-1)];
+}
+
+
+
+echo "Espèce d" ;
+animal();
+echo ' ';
+  lulz();
+  echo ' ';
+  lieu();
+
+?>
