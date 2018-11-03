970dfdaf81b9beda309ba4bfa8b3ab6ef9cf905d
BuildTools <unconfigured@null.spigotmc.org>
2018-11-03 11:56:27 +0100

    add index.php symlink, and some minor changes i forgot

---
+++ index.php
@@ -0,0 +1,1 @@
+insultotron.php
--- insultotron.php
+++ insultotron.php
@@ -16,6 +16,7 @@ function animal()
 	"e mamouth a poil ras",
 	"e mamouth",
 	"'huitre",
+	"'acouphène",
 	"e joueur de LoL"
 	);
 echo $array[mt_rand(0,count($array)-1)];
@@ -25,18 +26,17 @@ echo $array[mt_rand(0,count($array)-1)];
 function lulz()
 {
  $array = array(
+	"",
 	"moustachu",
 	"barbu",
 	"roux",
-	"psychopathe",
 	"reconditioné",
+	"foireux",
 	"chauve",
-	"shootéa l'ibuprofen",
+	"shooté a l'ibuprofen",
 	"nucléairement con",
-    "mangeur de mamouths",
-	"manchot",
-	"pyroclastique",
-	"associal"
+        "mangeur de mamouths",
+	"pyroclastique"
 	);
 	echo$array[mt_rand(0,count($array)-1)];
 }
@@ -44,17 +44,19 @@ function lulz()
 function lieu()
 {
  $array = array(
+	"du cul",
+	"des fesses",
 	"des iles",
 	"venant de mars",
 	"des galapagos",
 	"australien",
 	"de ta soeur",
 	"sumérien",
-	"grec",
 	"de l'espace",
 	"du Zimbabwe",
 	"italien",
 	"des antilles",
+	"des enfers",
 	"numériquement à la ramasse"
 //	"OMG c'est quoi ce bPAaaar !  ͫ͌ṵ̮̲̣͊͊̐̽̊̅̚͠t̹͈̩ͧ̆̐a̡ͪ͌ͬ̀̂̾̚i̳͙̒͊̎͋̉̐̚n̠̞̂̄̇ ͓͚̩̜ͅċ͔̜̳̗̭͖͑ͫ'̪͔̗͙̩͈̎̏̚ͅề̟̅̅ͧs̛͉͉̆t̫̥̆̇ͥ͐̊͞ ̒͐̑̈́̈ͣ̚͏̮͚̱̬̺q̸͇̮͔ȕ̮̦͇̜̝̌ͯ͗͡o̜ͮ̈̏̔̈́ỉ̤̟ͧͯ́͠ ̄c̪̞͛̄ͬ̿́e̼̦̥̗̒ ̼̯̬͈͚͍̼ͮ̒b̵͎͉͍͚ͪ̒ͩͭo͖̺ͨͫ̊ͮ̿ȑ̩̲͑̈́͂d̸̖̬̞̦̘̯̱͑e̞̖̍ͧ̏ͨ̒̀l̪̬͋̊͛͌ͧ̽ ̖̤̘̮͕͆̀͗ͩͯ̑ͅͅd̼̝̋̃ͮ̏ͫͭ̈e͉̮͚̠̫̯ͮ͂̈́̒̈ ̤̯̖̝̜̉̌A̧ͩ́ͧ̈́̂̚A͎̳̰̪͎͉A͚̪ͬͩ̈ͮ̓R͙̰͂̓̌ͮ̌ͯ͠G̝̰̺̺̪͆̉ͦ̈́͢H͕̋̔́̑͟ ̰̬͚̃͆̅̉͑̃̐ il me  ͢ ̶͓̜͗ ̳̹͇̱̼͈͕͆̃̿ ̴̀ͧͯ ̾ͧͪ͑҉̪͙͓̜̟̖ ̙̤̼̋͗ ̲̯͈̰̫͍ ̖̲͍ͬ̿̂̒̇̈ ̤̻̍̃͊ͨͫ̚ ͓͇̩̝̝̃ͅ ̧͉̥͔̞͓̥̝̊̇ ̳̫̊̽̑ ̎ ̜̯̻̗̖͎̘͂̂̃͛ ̺̟͍̙̔ͫͪͫ͛ͯ͝ ͬ̔ ̵̑̑ͯ́̀ͤ̓MANGȩ̸̴̼̟̘̪̖͎̹͚̬̹͙̹̤̹͍̤̫ͪͦ͆ͩ͊̎͊̕͘",
 	
