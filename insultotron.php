851f63c2e140e6b61d833587d7fde2fa3ae85ed2
BuildTools <unconfigured@null.spigotmc.org>
2018-11-03 12:00:29 +0100

    huntards mode

--- insultotron.php
+++ insultotron.php
@@ -1,76 +1,53 @@
 ﻿<?php
-function animal()
+function action()
 {
  $array = array(
-	"e Végéta",
-	"UNDEFINED",
-	"e renard",
-	"e poulpe",
-	"e caribou",
-	"e chouette",
-	"'ornythorinque",
-	"e papier toilette",
-	"e zombie nécrophage",
-	"e vélocipède",
-	"e castor",
-	"e mamouth a poil ras",
-	"e mamouth",
-	"'huitre",
-	"'acouphène",
-	"e joueur de LoL"
-	);
+        "abbat",
+        "tire sur",
+        "descend",
+        "blesse mortellement",
+        "vise par accident",
+        "perfore",
+        "crible de balles"
+        );
 echo $array[mt_rand(0,count($array)-1)];
 
 }
-
-function lulz()
+function cible()
 {
  $array = array(
-	"",
-	"moustachu",
-	"barbu",
-	"roux",
-	"reconditioné",
-	"foireux",
-	"chauve",
-	"shooté a l'ibuprofen",
-	"nucléairement con",
-        "mangeur de mamouths",
-	"pyroclastique"
+	"un vieu",
+	"un écolier",
+	"un randonneur",
+	"une automobile",
+	"un cycliste",
+	"un groupe de hippies",
+	"le chien d'un promeneur",
+	"la maire de la ville",
+	"un autre chasseur"
+	);
+echo $array[mt_rand(0,count($array)-1)];
+}
+
+function animal()
+{
+ $array = array(
+	" faisant",
+	"e galinette cendrée",
+	" cerf",
+	" chevreuil",
+	" sanglier",
+	"e biche",
 	);
 	echo$array[mt_rand(0,count($array)-1)];
 }
 
-function lieu()
-{
- $array = array(
-	"du cul",
-	"des fesses",
-	"des iles",
-	"venant de mars",
-	"des galapagos",
-	"australien",
-	"de ta soeur",
-	"sumérien",
-	"de l'espace",
-	"du Zimbabwe",
-	"italien",
-	"des antilles",
-	"des enfers",
-	"numériquement à la ramasse"
-//	"OMG c'est quoi ce bPAaaar !  ͫ͌ṵ̮̲̣͊͊̐̽̊̅̚͠t̹͈̩ͧ̆̐a̡ͪ͌ͬ̀̂̾̚i̳͙̒͊̎͋̉̐̚n̠̞̂̄̇ ͓͚̩̜ͅċ͔̜̳̗̭͖͑ͫ'̪͔̗͙̩͈̎̏̚ͅề̟̅̅ͧs̛͉͉̆t̫̥̆̇ͥ͐̊͞ ̒͐̑̈́̈ͣ̚͏̮͚̱̬̺q̸͇̮͔ȕ̮̦͇̜̝̌ͯ͗͡o̜ͮ̈̏̔̈́ỉ̤̟ͧͯ́͠ ̄c̪̞͛̄ͬ̿́e̼̦̥̗̒ ̼̯̬͈͚͍̼ͮ̒b̵͎͉͍͚ͪ̒ͩͭo͖̺ͨͫ̊ͮ̿ȑ̩̲͑̈́͂d̸̖̬̞̦̘̯̱͑e̞̖̍ͧ̏ͨ̒̀l̪̬͋̊͛͌ͧ̽ ̖̤̘̮͕͆̀͗ͩͯ̑ͅͅd̼̝̋̃ͮ̏ͫͭ̈e͉̮͚̠̫̯ͮ͂̈́̒̈ ̤̯̖̝̜̉̌A̧ͩ́ͧ̈́̂̚A͎̳̰̪͎͉A͚̪ͬͩ̈ͮ̓R͙̰͂̓̌ͮ̌ͯ͠G̝̰̺̺̪͆̉ͦ̈́͢H͕̋̔́̑͟ ̰̬͚̃͆̅̉͑̃̐ il me  ͢ ̶͓̜͗ ̳̹͇̱̼͈͕͆̃̿ ̴̀ͧͯ ̾ͧͪ͑҉̪͙͓̜̟̖ ̙̤̼̋͗ ̲̯͈̰̫͍ ̖̲͍ͬ̿̂̒̇̈ ̤̻̍̃͊ͨͫ̚ ͓͇̩̝̝̃ͅ ̧͉̥͔̞͓̥̝̊̇ ̳̫̊̽̑ ̎ ̜̯̻̗̖͎̘͂̂̃͛ ̺̟͍̙̔ͫͪͫ͛ͯ͝ ͬ̔ ̵̑̑ͯ́̀ͤ̓MANGȩ̸̴̼̟̘̪̖͎̹͚̬̹͙̹̤̹͍̤̫ͪͦ͆ͩ͊̎͊̕͘",
-	
-	);
-	echo $array[mt_rand(0,count($array)-1)];
-}
 
-
-
-echo "Espèce d" ;
-animal();
+echo "Un chasseur " ;
+action();
 echo ' ';
-  lulz();
-  echo ' ';
-  lieu();
+cible();
+echo ", en pensant viser un";
+animal();
 
 ?>
